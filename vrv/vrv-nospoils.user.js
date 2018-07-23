8a9d784c40aa0166d34acad8d16781391224fe2f
Chris Lockfort <clockfort@gmail.com>
2018-07-22 20:32:19 -0700

    Update vrv-nospoils.user.js

--- vrv/vrv-nospoils.user.js
+++ vrv/vrv-nospoils.user.js
@@ -4,16 +4,14 @@
 // @version      0.1
 // @description  Show VRV descriptions as greyish shadow text that needs to be selected with the mouse to be read properly
 // @author       Chris Lockfort
-// @match        https://vrv.co/watch/*/*
+// @match        https://vrv.co/*
 // @grant        none
 // @run-at document-idle
 // ==/UserScript==
 
-(function() {
-    'use strict';
+// @run-at       document-start
 
-    var desc_div = document.getElementsByClassName("description")[0];
-    if (desc_div) {
-        desc_div.parentNode.removeChild(desc_div);
-    }
-})();
+var css = document.createElement("style")
+css.type = "text/css"
+css.innerHTML = "#content .description{ opacity: 0; } #content .description:hover { opacity: 0.5; }"
+document.head.appendChild(css)
